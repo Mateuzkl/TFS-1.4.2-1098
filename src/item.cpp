ee4fcffecb78a7d868348183ecdf99a3bf0d898f
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2025-01-25 21:36:21 -0300

    Added Item Classifications and Tiers for xml and lua.

--- src/item.cpp
+++ src/item.cpp
@@ -526,6 +526,23 @@ Attr_ReadValue Item::readAttr(AttrTypes_t attr, PropStream& propStream)
 			break;
 		}
 
+		case ATTR_CLASSIFICATION: {
+			uint32_t classification;
+			if (!propStream.read<uint32_t>(classification)) {
+				return ATTR_READ_ERROR;
+			}
+			setIntAttr(ITEM_ATTRIBUTE_CLASSIFICATION, classification);
+			break;
+		}
+		case ATTR_TIER: {
+			uint32_t tier;
+			if (!propStream.read<uint32_t>(tier)) {
+				return ATTR_READ_ERROR;
+			}
+			setIntAttr(ITEM_ATTRIBUTE_TIER, tier);
+			break;
+		}
+
 		case ATTR_DEFENSE: {
 			int32_t defense;
 			if (!propStream.read<int32_t>(defense)) {
@@ -603,22 +620,6 @@ Attr_ReadValue Item::readAttr(AttrTypes_t attr, PropStream& propStream)
 			}
 
 			setIntAttr(ITEM_ATTRIBUTE_STOREITEM, storeItem);
-			break;
-		}
-
-		//12+ compatibility
-		case ATTR_OPENCONTAINER:
-		case ATTR_TIER: {
-			if (!propStream.skip(1)) {
-				return ATTR_READ_ERROR;
-			}
-			break;
-		}
-
-		case ATTR_PODIUMOUTFIT: {
-			if (!propStream.skip(15)) {
-				return ATTR_READ_ERROR;
-			}
 			break;
 		}
 
@@ -805,6 +806,15 @@ void Item::serializeAttr(PropWriteStream& propWriteStream) const
 	if (hasAttribute(ITEM_ATTRIBUTE_ATTACK_SPEED)) {
 		propWriteStream.write<uint8_t>(ATTR_ATTACK_SPEED);
 		propWriteStream.write<uint32_t>(getIntAttr(ITEM_ATTRIBUTE_ATTACK_SPEED));
+	}
+
+	if (hasAttribute(ITEM_ATTRIBUTE_CLASSIFICATION)) {
+		propWriteStream.write<uint8_t>(ATTR_CLASSIFICATION);
+		propWriteStream.write<uint32_t>(getIntAttr(ITEM_ATTRIBUTE_CLASSIFICATION));
+	}
+	if (hasAttribute(ITEM_ATTRIBUTE_TIER)) {
+		propWriteStream.write<uint8_t>(ATTR_TIER);
+		propWriteStream.write<uint32_t>(getIntAttr(ITEM_ATTRIBUTE_TIER));
 	}
 
 	if (hasAttribute(ITEM_ATTRIBUTE_DEFENSE)) {
@@ -1027,6 +1037,13 @@ std::string Item::getDescription(const ItemType& it, int32_t lookDistance,
 			}
 		}
 
+		// Show Classification and Tier on item 
+		uint32_t classification = item ? item->getClassification() : it.classification;
+		uint32_t tier = item ? item->getTier() : it.tier;
+		if (classification) {
+			s << "\nClassification: " << classification << " Tier: " << tier;
+		}
+
 		if (it.abilities) {
 			for (uint8_t i = SKILL_FIRST; i <= SKILL_LAST; i++) {
 				if (!it.abilities->skills[i]) {
@@ -1183,6 +1200,13 @@ std::string Item::getDescription(const ItemType& it, int32_t lookDistance,
 		if (armor != 0) {
 			s << " (Arm:" << armor;
 			begin = false;
+		}
+
+		// Show Classification and Tier on item 
+		uint32_t classification = item ? item->getClassification() : it.classification;
+		uint32_t tier = item ? item->getTier() : it.tier;
+		if (classification) {
+			s << "\nClassification: " << classification << " Tier: " << tier;
 		}
 
 		if (it.abilities) {
--- src/luascript.cpp
+++ src/luascript.cpp
@@ -1434,6 +1434,9 @@ void LuaScriptInterface::registerEnums()
 	registerEnum(ITEM_ATTRIBUTE_PLURALNAME)
 	registerEnum(ITEM_ATTRIBUTE_WEIGHT)
 	registerEnum(ITEM_ATTRIBUTE_ATTACK)
+	registerEnum(ITEM_ATTRIBUTE_ATTACK_SPEED)
+	registerEnum(ITEM_ATTRIBUTE_CLASSIFICATION)
+	registerEnum(ITEM_ATTRIBUTE_TIER)
 	registerEnum(ITEM_ATTRIBUTE_DEFENSE)
 	registerEnum(ITEM_ATTRIBUTE_EXTRADEFENSE)
 	registerEnum(ITEM_ATTRIBUTE_ARMOR)
@@ -1449,7 +1452,6 @@ void LuaScriptInterface::registerEnums()
 	registerEnum(ITEM_ATTRIBUTE_DECAYTO)
 	registerEnum(ITEM_ATTRIBUTE_WRAPID)
 	registerEnum(ITEM_ATTRIBUTE_STOREITEM)
-	registerEnum(ITEM_ATTRIBUTE_ATTACK_SPEED)
 
 	registerEnum(ITEM_TYPE_DEPOT)
 	registerEnum(ITEM_TYPE_MAILBOX)
@@ -2754,6 +2756,8 @@ void LuaScriptInterface::registerFunctions()
 
 	registerMethod("ItemType", "getAttack", LuaScriptInterface::luaItemTypeGetAttack);
 	registerMethod("ItemType", "getAttackSpeed", LuaScriptInterface::luaItemTypeGetAttackSpeed);
+	registerMethod("ItemType", "getClassification", LuaScriptInterface::luaItemTypeGetClassification);
+	registerMethod("ItemType", "getTier", LuaScriptInterface::luaItemTypeGetTier);
 	registerMethod("ItemType", "getDefense", LuaScriptInterface::luaItemTypeGetDefense);
 	registerMethod("ItemType", "getExtraDefense", LuaScriptInterface::luaItemTypeGetExtraDefense);
 	registerMethod("ItemType", "getArmor", LuaScriptInterface::luaItemTypeGetArmor);
@@ -12136,6 +12140,31 @@ int LuaScriptInterface::luaItemTypeGetAttackSpeed(lua_State* L)
 	if (itemType) {
 		lua_pushnumber(L, itemType->attackSpeed);
 	} else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+
+int LuaScriptInterface::luaItemTypeGetClassification(lua_State* L)
+{
+	// itemType:getClassification()
+	const ItemType* itemType = getUserdata<const ItemType>(L, 1);
+	if (itemType) {
+		lua_pushnumber(L, itemType->classification);
+	}
+	else {
+		lua_pushnil(L);
+	}
+	return 1;
+}
+int LuaScriptInterface::luaItemTypeGetTier(lua_State* L)
+{
+	// itemType:getTier()
+	const ItemType* itemType = getUserdata<const ItemType>(L, 1);
+	if (itemType) {
+		lua_pushnumber(L, itemType->tier);
+	}
+	else {
 		lua_pushnil(L);
 	}
 	return 1;
